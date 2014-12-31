9fcacc45ac1004a759810361e1f167432e3f1284
Mark Kennedy <mark.kennedy@akqa.com>
2014-12-31 00:23:13 -0500

    Fixing demo page

--- demo/demo.js
+++ demo/demo.js
@@ -1,37 +1,34 @@
-require([
-    '../build/utils',
-    '../build/checkbox',
-    '../build/button-toggle',
-    '../build/input-field'
-], function (
-    Utils,
-    Checkbox,
-    ButtonToggle,
-    InputField
-) {
+requirejs.config({
+    baseUrl: '../src',
+    'paths': {
+        'underscore': '../external/underscore/underscore-min',
+        'element-kit': '../external/element-kit/element-kit.min'
+    }
+});
+
+require(['form'], function (Form) {
     'use strict';
 
     var SampleView = function () {
 
 
-        this.checkbox = new Checkbox({
+        this.checkbox = new Form.Checkbox({
             el: document.getElementsByClassName('ui-checkbox-input')[0]
         });
 
         var checkboxToggleContainer = document.getElementsByClassName('multi-select-button-toggle')[0];
-        this.multiSelectToggle = new ButtonToggle({
+        this.multiSelectToggle = new Form.ButtonToggle({
             inputs: checkboxToggleContainer.getElementsByClassName('ui-button-toggle-input')
         });
 
         var radioToggleContainer = document.getElementsByClassName('single-select-button-toggle')[0];
-        this.singleSelectToggle = new ButtonToggle({
+        this.singleSelectToggle = new Form.ButtonToggle({
             inputs: radioToggleContainer.getElementsByClassName('ui-button-toggle-input')
         });
 
-        this.inputField = new InputField({
+        this.inputField = new Form.InputField({
             el: document.getElementsByClassName('name-input-field')[0]
         });
-
 
     };
 
