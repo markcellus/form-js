6ae94c03138b93c2d568de30eb874f2ad3ad9a5a
Mark Kennedy <mark.kennedy@akqa.com>
2015-03-19 00:06:21 -0400

    Broke out modules, using browserify now + using upgraded build tools

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,90 +1,8 @@
 module.exports = function(grunt) {
     "use strict";
 
-    var banner = '/** \n' +
-        '* FormJS - v<%= pkg.version %>.\n' +
-        '* <%= pkg.repository.url %>\n' +
-        '* Copyright <%= grunt.template.today("yyyy") %>. Licensed MIT.\n' +
-        '*/\n';
-
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
-        clean: ['dist'],
-        copy: {
-            all: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'bower_components/sinonjs',
-                        dest: 'external/sinon',
-                        src: ['sinon.js']
-                    },
-                    {
-                        expand: true,
-                        cwd: 'bower_components/qunit/qunit',
-                        dest: 'external/qunit',
-                        src: ['qunit.js', 'qunit.css']
-                    },
-                    {
-                        expand: true,
-                        cwd: 'bower_components/element-kit/dist',
-                        dest: 'external/element-kit',
-                        src: ['element-kit.min.js']
-                    },
-                    {
-                        expand: true,
-                        cwd: 'bower_components/requirejs',
-                        dest: 'external/requirejs',
-                        src: ['require.js']
-                    },
-                    {
-                        expand: true,
-                        cwd: 'bower_components/underscore',
-                        dest: 'external/underscore',
-                        src: ['underscore-min.js']
-                    }
-                ]
-            },
-            src: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'src',
-                        dest: 'dist',
-                        src: ['form.js']
-                    }
-                ]
-            }
-        },
-        uglify: {
-            my_target: {
-                files: {
-                    'dist/form.min.js': ['dist/form.js']
-                }
-            }
-        },
-        connect: {
-            test: {
-                options: {
-                    hostname: 'localhost',
-                    port: 7000
-                }
-            },
-            local: {
-                options: {
-                    keepalive: true
-                }
-            }
-        },
-        qunit: {
-            local: {
-                options: {
-                    urls: [
-                        'http://localhost:7000/tests/index.html'
-                    ]
-                }
-            }
-        },
         jsdoc : {
             dist : {
                 src: ['src/**/*.js'],
@@ -102,71 +20,33 @@ module.exports = function(grunt) {
                 src: '_site-docs'
             }
         },
-        release: {
-            options: {
-                additionalFiles: ['bower.json'],
-                tagName: 'v<%= version %>',
-                commitMessage: 'release <%= version %>',
-                npm: false
-            }
-        },
-        usebanner: {
-            all: {
-                options: {
-                    banner: banner,
-                    linebreak: false
-                },
+        bt: {
+            dist: 'dist',
+            uglify: {
                 files: {
-                    src: [
-                        'dist/**/*'
-                    ]
+                    'dist/form-min.js': ['dist/form.js']
+                }
+            },
+            browserify: {
+                files: {
+                    'dist/form.js': ['src/**/*.js']
+                },
+                options: {
+                    browserifyOptions: {
+                        standalone: 'Form'
+                    }
+
+                }
+            },
+            tests: {
+                qunit: {
+                    src: ['tests/*.js']
                 }
             }
-        },
-        bump: {
-            options: {
-                files: ['package.json', 'bower.json'],
-                commit: false,
-                createTag: false,
-                push: false,
-                updateConfigs: ['pkg']
-            }
         }
     });
 
-    // Load grunt tasks from node modules
-    require( "load-grunt-tasks" )( grunt , {
-        loadGruntTasks: {
-            pattern: 'grunt-*'
-        }
-    });
-
-    grunt.task.registerTask('release', 'A custom release.', function(type) {
-        type = type || 'patch';
-        grunt.task.run([
-            'bump:' + type,
-            'build'
-        ]);
-    });
-
-    // Default grunt
-    grunt.registerTask( "build", [
-        "clean",
-        "copy:all",
-        "copy:src",
-        "uglify",
-        "usebanner:all",
-        "test"
-    ]);
-
-    grunt.registerTask( "server", [
-        "connect:local"
-    ]);
-
-    grunt.registerTask( "test", [
-        "connect:test",
-        "qunit"
-    ]);
+    require("load-grunt-tasks")(grunt);
 
     grunt.registerTask("publish_docs", [
         "jsdoc",
---
+++ src/form-element.js
@@ -0,0 +1,101 @@
+'use strict';
+var _ = require('underscore');
+require('element-kit');
+
+/**
+ * @class FormElement
+ * @description An extendable base class that provides common functionality among all form elements.
+ * @param {Object} options - Instantiation options
+ */
+var FormElement = function (options) {
+    this.initialize(options);
+};
+
+FormElement.prototype = /** @lends FormElement.prototype */{
+
+    /**
+     * Sets up stuff.
+     * @abstract
+     */
+    initialize: function (options) {
+        this.options = options || {};
+    },
+
+    /**
+     * Gets the form element.
+     * @returns {HTMLElement} Returns the form element
+     * @abstract
+     */
+    getFormElement: function () {
+        return this.options.el;
+    },
+
+    /**
+     * Gets the ui version of the form element.
+     * @returns {HTMLElement} Returns the ui-version of the element.
+     * @abstract
+     */
+    getUIElement: function () {
+        return this.getFormElement();
+    },
+
+    /**
+     * Gets the form elements.
+     * @returns {Array} Returns the array of form elements
+     * @abstract
+     */
+    getFormElements: function () {
+        return [this.getFormElement()];
+    },
+
+    /**
+     * Gets the current value of the element.
+     * @returns {string}
+     */
+    getValue: function () {
+        return this.getFormElement().value;
+    },
+
+    /**
+     * Gets the ui versions of the form elements.
+     * @returns {Array} Returns the array of ui-versions of the element.
+     * @abstract
+     */
+    getUIElements: function () {
+        return [this.getUIElement()];
+    },
+
+    /**
+     * Enables the form element.
+     * @abstract
+     */
+    enable: function () {
+        this.getFormElement().disabled = false;
+    },
+
+    /**
+     * Disables the form element.
+     * @abstract
+     */
+    disable: function () {
+        this.getFormElement().disabled = true;
+    },
+
+    /**
+     * Gets the element's identifier (preferably unique from all other elements that extend this class).
+     * @returns {string} Return the unique key
+     * @abstract
+     */
+    getElementKey: function () {
+        return 'element';
+    },
+
+    /**
+     * Destruction of this class.
+     * @abstract
+     */
+    destroy: function () {}
+
+};
+
+module.exports = FormElement;
