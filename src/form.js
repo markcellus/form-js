35412e10c6610f1b8a016eaaadb40cfe6d58666e
Mark Kennedy <mark.kennedy@akqa.com>
2015-03-27 21:02:43 -0400

    fixing documentation

--- src/form.js
+++ src/form.js
@@ -26,7 +26,7 @@ require('element-kit');
  * @param {object} options - The options
  * @param {HTMLFormElement} [options.el] - The form element
  * @param {HTMLFormElement} options.el - The form element
- * @param {Form~onValueChange} [options.onValueChange] - A callback function that fires when the input value changes
+ * @param {Form~onValueChange} [options.onValueChange] - A callback function that fires when the value of any form element changes
  * @param {Function} [options.onGetOptions] - Function callback that is fired upon instantiation to provide custom options
  * @param {string} [options.dropdownClass] - The css class used to query the set of dropdown elements that should be included
  * @param {string} [options.checkboxClass] - The css class used to query the set of checkbox elements that should be included
